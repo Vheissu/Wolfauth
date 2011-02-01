02acfaff48ef8089de947d8c4bf7e01366a20190
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-01 13:50:34 +1000

    Driver based Auth is not going to be happening yet.

--- libraries/WolfAuth/WolfAuth_Core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-abstract class WolfAuth_Core {
-    
-    abstract protected function login();
-    abstract protected function logout();
-    abstract protected function get_role();
-    
-}
