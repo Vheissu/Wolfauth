18684e481a73fc026b9ee281c88df4063b84bc06
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-09 14:05:10 +1000

    Added WolfAuth email language file.

---
+++ language/english/wolfauth_email_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['user_activation_subject'] = "Please confirm your at {site_name} account";
+$lang['user_activated_subject']  = "Your {site_name} account has been successfully activated";
