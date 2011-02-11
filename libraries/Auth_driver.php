cbeb510fd2c06ad6f506dc7632758b9a43806a66
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-12 01:27:34 +1000

    Messing with drivers a bit. Added in authexample controller for testing drivers functionality.

--- libraries/Auth_driver.php
+++ libraries/Auth_driver.php
@@ -11,9 +11,55 @@
 
 class Auth_driver extends CI_Driver_Library {
     
+    protected $_session_data;
+    protected $_user_data;
+    
     public function __construct()
     {
+        // Set the valid drivers
         $this->valid_drivers = array('auth_driver_facebook');
+        
+        // Don't overwrite user data
+        if (empty($this->_user_data))
+        {
+            $this->_user_data = array();
+        }
+    }
+    
+    /**
+    * Logs a user in depending on criteria
+    * 
+    */
+    public function login()
+    {
+        
+    }
+    
+    /**
+    * Log a user out depending on criteria
+    * 
+    */
+    public function logout()
+    {
+        
+    }
+    
+    /**
+    * Allows child classes (drivers) to create sessions
+    * 
+    */
+    protected function _set_session()
+    {
+        
+    }
+    
+    /**
+    * Allows child classes (drivers) to store user data
+    * 
+    */
+    protected function _set_user_data()
+    {
+        
     }
     
 }
