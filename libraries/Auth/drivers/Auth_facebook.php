f1c8c71bc63b9df9dbae8ea0528ba43d8b23d8a1
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-04-08 08:10:20 +1000

    Started work on the Facebook driver by implementing a login function.

--- libraries/Auth/drivers/Auth_facebook.php
+++ libraries/Auth/drivers/Auth_facebook.php
@@ -21,6 +21,8 @@ class Auth_facebook extends CI_Driver {
     protected $_ci;
     protected $config;
     
+    protected $user_info;
+    
     public function __construct()
     {
         // Load some things
@@ -32,9 +34,36 @@ class Auth_facebook extends CI_Driver {
         $this->_ci->lang->load('auth');
         $this->_ci->load->library('session');
         $this->_ci->load->model('auth/user_model');
+        $this->_ci->load->model('auth/facebook_model');
         
         // Get the config values for this driver defined in the config/auth.php file
         $this->config = (object)$this->_ci->config->item('facebook');        
+    }
+    
+    /**
+    * Login with your Facebook account, sir.
+    * 
+    */
+    public function login()
+    {
+        // Get the Facebook user
+        $fb_user = $this->_ci->facebook_model->get_user();
+        
+        // Looks like we have ourselves a Facebook user here
+        if ( $fb_user )
+        {
+            // Store the Facebook user ID
+            $this->_ci->session->set_userdata('facebook_id', $fb_user['id']);
+            
+            // Look through the database to see if this user has Facebooked before
+            $user = $this->_ci->user_model->get_user("facebook_id", $fb_user['id']);
+            
+            // User was found aiiiighhttt
+            if ( $user )
+            {
+                
+            }
+        }        
     }
     
 }
