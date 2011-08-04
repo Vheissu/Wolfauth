656d1e73ae221c17d186d9c810d0f1b086cc0008
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-08-05 08:31:23 +1000

    Added in start of Facebook driver.

---
+++ libraries/Auth/drivers/Auth_Facebook.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * WolfAuth
+ *
+ * An open source driver based authentication library for Codeigniter
+ * 
+ * This driver allows user accounts to be created, login users by Facebook
+ * and a whole lot more Facebook related goodness.
+ *
+ * @package       WolfAuth
+ * @subpackage    Facebook
+ * @author        Dwayne Charrington
+ * @copyright     Copyright (c) 2011 Dwayne Charrington.
+ * @link          http://ilikekillnerds.com
+ * @license       http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+class Auth_Facebook extends CI_Driver {
+    
+    protected $auth;
+    protected $ci;
+    protected $config;
+
+    public function __construct()
+    {
+        $this->auth = auth_instance();
+        $this->ci   = get_instance();
+    }
+    
+}
