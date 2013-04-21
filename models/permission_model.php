01d94587da140f8ddb17c3532222d0b6e0d0d456
Dwayne Charrington <dwayencharrington@gmail.com>
2013-04-22 08:13:29 +1000

    More commenting, isn't this fun?

--- models/permission_model.php
+++ models/permission_model.php
@@ -2,6 +2,12 @@
 
 class Permission_model extends CI_Model {
 
+	/**
+	 * Get Permissions
+	 *
+	 * Get all valid permissions from the database
+	 *
+	 */
 	public function get_permissions()
 	{
 		$permissions = $this->db->get('roles');
@@ -9,6 +15,14 @@ class Permission_model extends CI_Model {
 		return ($permissions->num_rows() >= 1) ? $permissions->result() : FALSE;
 	}
 
+
+	/**
+	 * Get Role Permissions
+	 *
+	 * Get all permissions for a particular role
+	 *
+	 *
+	 */
 	public function get_role_permissions($role_id)
 	{
 		$role = $this->db->get_where('roles', array('id' => $role_id), 1, 0);
